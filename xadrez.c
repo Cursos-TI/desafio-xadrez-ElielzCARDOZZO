1c792444d90ae6d0b9a01092a74b19e1520b5df3
ElielzCARDOZZO <naraske303@gmail.com>
2025-06-10 15:35:45 -0400

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -29,9 +29,39 @@ int main() {
     // UTILIZAÇÃO DAS ESTRUTURA DE REPETIÇÃO DO-WHILE E SUA CONDICIONAL
     printf("\n A RAINHA: \n");
     do {
-        printf("A RAINHA moveu-se para a ESQUERDA!\n");
+        printf("A RAINHA moveu-se para a ESQUERDA!\n\n");
         i++;
     } while (i < 8);
 
+
+    // IMPLEMENTAÇÃO DA PEÇA CAVALO NO CÓDIGO EM C, ADICIONANDO DUAS CASAS A BAIXO E UMA A ESQUERDA ---
+    // (DE ACORDO COM O DESAFIO DO NÍVEL AVENTUREIRO)
+    printf("MOVIMENTAÇÃO DA PEÇA DE XADREZ: C A V A L O ! ! !\n");
+
+        for (i = 0; i < 2; i++) //ESTRUTURA DE REPETIÇÃO FOR USADO PARA REPETIR DUAS CASAS ABAIXO
+        {
+            printf("BAIXO!\n"); //VISUALIZAÇÃO DA MENSAGEM DE LOMOCOMOÇÃO DA PEÇA CAVALO
+
+            while (i == 1) //ESTRUTURA DE REPETIÇÃO WHILE USADO PARA REPETIR UMA CASA A ESQUERDA
+            {
+                printf("ESQUERDA!\n\n"); //VISUALIZAÇÃO DA MENSAGEM DE LOMOCOMOÇÃO DA PEÇA CAVALO
+                i++; 
+            }
+            
+        }
+
+                printf("--------------------------------------------------------------------------------\n");
+
+        for (i = 0; i < 2; i++) //ESTRUTURA DE REPETIÇÃO FOR USADO PARA REPETIR DUAS CASAS PARA CIMA
+        {
+            printf("CIMA!\n"); //VISUALIZAÇÃO DA MENSAGEM DE LOMOCOMOÇÃO DA PEÇA CAVALO
+
+            while (i == 1) //ESTRUTURA DE REPETIÇÃO WHILE USADO PARA REPETIR UMA CASA A DIREITA
+            {
+                printf("DIREITA!\n"); //VISUALIZAÇÃO DA MENSAGEM DE LOMOCOMOÇÃO DA PEÇA CAVALO
+                i++; 
+            }
+            
+        }
     return 0;
 }
